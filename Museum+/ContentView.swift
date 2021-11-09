ec62d6df168a4847665f750400eb893c66347cca
unclechuen <33083306+unclechuen@users.noreply.github.com>
2021-11-09 22:10:53 +0800

    Initial Commit

---
+++ Museum+/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Museum+
+//
+//  Created by Johnny on 9/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Museum+/Museum_App.swift
@@ -0,0 +1,17 @@
+//
+//  Museum_App.swift
+//  Museum+
+//
+//  Created by Johnny on 9/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Museum_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
